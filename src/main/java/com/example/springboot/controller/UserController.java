0c8f171653708674f18e93ef478e7121489ed3df
Md. Atiqul Islam <air.cse.pbn@gmail.com>
2023-08-10 11:33:46 +0600

    Add update user

--- src/main/java/com/example/springboot/controller/UserController.java
+++ src/main/java/com/example/springboot/controller/UserController.java
@@ -5,10 +5,9 @@ import com.example.springboot.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/users")
@@ -20,5 +19,23 @@ public class UserController {
     public ResponseEntity<User> createUser(@RequestBody User user){
         User savedUser = service.createUser(user);
         return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<User> getUserById(@PathVariable("id") Long userId){
+        User user = service.getUserById(userId);
+        return ResponseEntity.ok(user);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUser(){
+        return ResponseEntity.ok(service.getAllUser());
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<User> updateUser(@RequestBody User user, @PathVariable("id") Long userId){
+        user.setId(userId);
+        User updatedUser = service.updateUser(user);
+        return ResponseEntity.ok(updatedUser);
     }
 }
--- src/main/java/com/example/springboot/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/springboot/service/impl/UserServiceImpl.java
@@ -6,6 +6,9 @@ import com.example.springboot.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 @AllArgsConstructor
 public class UserServiceImpl implements UserService {
@@ -14,5 +17,26 @@ public class UserServiceImpl implements UserService {
     @Override
     public User createUser(User user) {
         return repository.save(user);
+    }
+
+    @Override
+    public User getUserById(Long userId) {
+        Optional<User> optionalUser = repository.findById(userId);
+        return optionalUser.get();
+    }
+
+    @Override
+    public List<User> getAllUser() {
+        return repository.findAll();
+    }
+
+    @Override
+    public User updateUser(User user) {
+        User existingUser = repository.findById(user.getId()).get();
+        existingUser.setFirstName(user.getFirstName());
+        existingUser.setLastName(user.getLastName());
+        existingUser.setEmail(user.getEmail());
+        User updatedUser = repository.save(existingUser);
+        return updatedUser;
     }
 }
